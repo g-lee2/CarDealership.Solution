65492cc7f4718380b8cb195b5bfa1eba79c06dd2
Grace Lee <gracexsnoopy@gmail.com>
2022-11-30 20:43:11 -0800

    Update route and update cshtml view to use razor syntax

--- CarDealership/Controllers/HomeController.cs
+++ CarDealership/Controllers/HomeController.cs
@@ -1,12 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using CarDealership.Models;
 
 namespace CarDealership.Controllers
 {
   public class HomeController : Controller
   {
     [Route("/")]
-    public ActionResult Dealership() { return View(); }
-
-
+    public ActionResult Dealership()
+    {
+      CarVariable myCarVariable = new CarVariable();
+      myCarVariable.MakeModel = "1974 Volkswagen Thing";
+      myCarVariable.Price = 1100;
+      myCarVariable.Miles = 368792;
+      return View(myCarVariable);
+    }
   }
 }
