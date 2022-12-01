d201962e822f4565c47ffca52907356386ecfb7c
Grace Lee <gracexsnoopy@gmail.com>
2022-11-30 20:27:52 -0800

    Add auto-implemented properties

--- CarDealership/Controllers/HomeController.cs
+++ CarDealership/Controllers/HomeController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CarDealership.Controllers
 {
-  public class DealershipController : Controller
+  public class HomeController : Controller
   {
     [Route("/")]
     public ActionResult Dealership() { return View(); }
---
+++ CarDealership/Models/CarVariable.cs
@@ -0,0 +1,9 @@
+namespace CarDealership.Models
+{
+  public class CarVariable
+  {
+    public string MakeModel { get; set; }
+    public int Price { get; set; }
+    public int Miles { get; set; }
+  }
+}
